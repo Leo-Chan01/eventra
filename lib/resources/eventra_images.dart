11a5f68f42e37247de6ee776c3d91a02854f1544
Leo-Chan01 <54512508+Leo-Chan01@users.noreply.github.com>
2026-04-11 08:34:37 -0700

    feat: add new category images and update home UI with new categories

--- lib/resources/eventra_images.dart
+++ lib/resources/eventra_images.dart
@@ -6,25 +6,59 @@ class EventraImages {
   static const String appIcon = 'assets/images/app-icon.png';
   static const String bartenderPreparingDrinkBar1 =
       'assets/images/bartender-preparing-drink-bar 1.png';
+  static const String bouncersCategoryImage =
+      'assets/images/bouncers-category-image.png';
   static const String cakesCategoryIcon =
       'assets/images/cakes_category_icon.png';
   static const String cameramanImage = 'assets/images/cameraman-image.png';
+  static const String catererCategoryImage =
+      'assets/images/caterer-category-image.png';
+  static const String contentCreatorCategoryImage =
+      'assets/images/content-creator-category-image.png';
   static const String decoratorPerson = 'assets/images/decorator-person.png';
   static const String decoratorCategoryIcon =
       'assets/images/decorator_category_icon.png';
+  static const String djCategoryImage = 'assets/images/dj-category-image.png';
+  static const String drinksCategoryImage =
+      'assets/images/drinks-category-image.png';
+  static const String eventplannerCategoryImage =
+      'assets/images/eventplanner-category-image.png';
   static const String femaleWeddingPlannerWorkingCeremony1 =
       'assets/images/female-wedding-planner-working-ceremony 1.png';
+  static const String hairStylistCategoryImage =
+      'assets/images/hair-stylist-category-image.png';
+  static const String hallCategoryImage =
+      'assets/images/hall-category-image.png';
   static const String headerCarouselImage =
       'assets/images/header-carousel-image.png';
+  static const String ligntningCategoryImage =
+      'assets/images/ligntning-category-image.png';
+  static const String liveBandCategoryImage =
+      'assets/images/live-band-category-image.png';
   static const String logoIconSmall = 'assets/images/logo-icon-small.png';
   static const String logoText = 'assets/images/logo-text.png';
+  static const String makeUpCategoryImage =
+      'assets/images/make-up-category-image.png';
+  static const String mcCategoryImage = 'assets/images/mc-category-image.png';
   static const String onboardingImage1 = 'assets/images/onboarding_image_1.png';
   static const String onboardingImage2 = 'assets/images/onboarding_image_2.png';
   static const String onboardingImgae3 = 'assets/images/onboarding_imgae_3.png';
+  static const String photographyCategoryImage =
+      'assets/images/photography-category-image.png';
   static const String profileDummy = 'assets/images/profile-dummy.png';
   static const String rentalCategoryIcon =
       'assets/images/rental_category_icon.png';
+  static const String smallChopsCategoryImage =
+      'assets/images/small-chops-category-image.png';
+  static const String usherCategoryImage =
+      'assets/images/usher-category-image.png';
+  static const String videographerCategoryImage =
+      'assets/images/videographer-category-image.png';
+  static const String weddingDressCategoryImage =
+      'assets/images/wedding-dress-category-image.png';
   static const String weddingImage = 'assets/images/wedding-image.png';
+  static const String weddingStylistCategoryImage =
+      'assets/images/wedding-stylist-category-image.png';
   static const String womanWithfoodplate =
       'assets/images/woman-withfoodplate.png';
   static const String womanWithweddinggown =
--- test/eventra_images_test.dart
+++ test/eventra_images_test.dart
@@ -10,23 +10,47 @@ void main() {
       File(EventraImages.bartenderPreparingDrinkBar1).existsSync(),
       isTrue,
     );
+    expect(File(EventraImages.bouncersCategoryImage).existsSync(), isTrue);
     expect(File(EventraImages.cakesCategoryIcon).existsSync(), isTrue);
     expect(File(EventraImages.cameramanImage).existsSync(), isTrue);
+    expect(File(EventraImages.catererCategoryImage).existsSync(), isTrue);
+    expect(
+      File(EventraImages.contentCreatorCategoryImage).existsSync(),
+      isTrue,
+    );
     expect(File(EventraImages.decoratorPerson).existsSync(), isTrue);
     expect(File(EventraImages.decoratorCategoryIcon).existsSync(), isTrue);
+    expect(File(EventraImages.djCategoryImage).existsSync(), isTrue);
+    expect(File(EventraImages.drinksCategoryImage).existsSync(), isTrue);
+    expect(File(EventraImages.eventplannerCategoryImage).existsSync(), isTrue);
     expect(
       File(EventraImages.femaleWeddingPlannerWorkingCeremony1).existsSync(),
       isTrue,
     );
+    expect(File(EventraImages.hairStylistCategoryImage).existsSync(), isTrue);
+    expect(File(EventraImages.hallCategoryImage).existsSync(), isTrue);
     expect(File(EventraImages.headerCarouselImage).existsSync(), isTrue);
+    expect(File(EventraImages.ligntningCategoryImage).existsSync(), isTrue);
+    expect(File(EventraImages.liveBandCategoryImage).existsSync(), isTrue);
     expect(File(EventraImages.logoIconSmall).existsSync(), isTrue);
     expect(File(EventraImages.logoText).existsSync(), isTrue);
+    expect(File(EventraImages.makeUpCategoryImage).existsSync(), isTrue);
+    expect(File(EventraImages.mcCategoryImage).existsSync(), isTrue);
     expect(File(EventraImages.onboardingImage1).existsSync(), isTrue);
     expect(File(EventraImages.onboardingImage2).existsSync(), isTrue);
     expect(File(EventraImages.onboardingImgae3).existsSync(), isTrue);
+    expect(File(EventraImages.photographyCategoryImage).existsSync(), isTrue);
     expect(File(EventraImages.profileDummy).existsSync(), isTrue);
     expect(File(EventraImages.rentalCategoryIcon).existsSync(), isTrue);
+    expect(File(EventraImages.smallChopsCategoryImage).existsSync(), isTrue);
+    expect(File(EventraImages.usherCategoryImage).existsSync(), isTrue);
+    expect(File(EventraImages.videographerCategoryImage).existsSync(), isTrue);
+    expect(File(EventraImages.weddingDressCategoryImage).existsSync(), isTrue);
     expect(File(EventraImages.weddingImage).existsSync(), isTrue);
+    expect(
+      File(EventraImages.weddingStylistCategoryImage).existsSync(),
+      isTrue,
+    );
     expect(File(EventraImages.womanWithfoodplate).existsSync(), isTrue);
     expect(File(EventraImages.womanWithweddinggown).existsSync(), isTrue);
   });
